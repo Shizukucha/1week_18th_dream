96ccf5f2d644f7f26dacdbb07d318aef1312f7d9
Junegames <junegames0107@gmail.com>
2021-10-03 20:05:56 +0900

    BGM・SE音量調整の仮実装、サキュバスが弾を打つ挙動や弾自体の挙動の追加。

---
+++ Assets/JUN_Assets/JUN_Script/EnemyBullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+   // [SerializeField] float bulletSpeed;
+    [SerializeField] float bulletSize;
+    [SerializeField] float bulletSpeed;
+
+    float dx; // X方向
+    float dy; // Y方向
+
+    
+
+    // 上に動く
+    private void Update()
+    {
+
+        
+        transform.localScale = new Vector3(bulletSize, bulletSize, 0);
+
+        transform.position += new Vector3(dx, dy, 0) * Time.deltaTime ;
+        //一定の範囲を超えると破壊。
+        /*
+        if(transform.position.y > 3)
+        {
+            Destroy(gameObject);
+        }
+       */
+    }
+
+    public void BulletSetting(float angle, float speed )
+    {
+        dx = Mathf.Cos(angle) * bulletSpeed;
+        dy = Mathf.Sin(angle) * bulletSpeed;
+    }
+
+}
---
+++ Assets/JUN_Assets/JUN_Script/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, -0.001f, 0);
+        if (transform.position.y <  -4.74f)
+        {
+            transform.position = new Vector3(0, 4.74f, 0);
+        }
+
+    }
+}
